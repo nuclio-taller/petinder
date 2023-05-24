f2116e03ddad99dce626e3b75a4822b5c23a4666
Jordi Galobart Salvat <jgalobart@gmail.com>
2023-05-24 10:03:36 +0200

    change app port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const axios = require('axios');
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 8000
 
 async function getPetImage() {
 
